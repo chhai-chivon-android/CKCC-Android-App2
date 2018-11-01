a5b716d2b10289019abdffd56d6437c12061c4e7
leapkh <leapkh@yahoo.com>
2018-11-01 20:27:50 +0700

    Add data to Firestore.

--- app/src/main/java/com/leapkh/ckcc/AddEventActivity.java
+++ app/src/main/java/com/leapkh/ckcc/AddEventActivity.java
@@ -1,10 +1,12 @@
 package com.leapkh.ckcc;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -14,6 +16,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,11 +27,14 @@ import java.util.Map;
 
 public class AddEventActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_event);
+        progressBar = findViewById(R.id.progressBar);
     }
 
     public void onSaveButtonClick(View view) {
@@ -67,7 +76,41 @@ public class AddEventActivity extends AppCompatActivity {
         */
 
         // Add event to Firestore
-        
+        progressBar.setVisibility(View.VISIBLE);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> event = new HashMap<>();
+        event.put("title", title);
+        event.put("date", date);
+        event.put("location", location);
+        event.put("imageUrl", "http://test.js-cambodia.com/ckcc/images/android.png");
+
+        /*db.collection("events").add(event).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(AddEventActivity.this, "Add event success", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(AddEventActivity.this, "Add event fail: " + task.getException(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });*/
+
+
+        db.collection("events").document("2").set(event).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(AddEventActivity.this, "Add event success", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(AddEventActivity.this, "Add event fail: " + task.getException(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
 }
