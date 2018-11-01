6bef0230d60ed67577431ae637aa3dc155f9f4ce
leapkh <leapkh@yahoo.com>
2018-11-01 16:19:07 +0700

    Initial commit

---
+++ app/src/main/java/com/leapkh/ckcc/EventsFragment.java
@@ -0,0 +1,108 @@
+package com.leapkh.ckcc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+public class EventsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View home = inflater.inflate(R.layout.fragment_events,container,false);
+        return home;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        final EventAdapter eventAdapter = new EventAdapter();
+        recyclerView.setAdapter(eventAdapter);
+
+        //Load data
+        String url = "http://test.js-cambodia.com/ckcc/events.php";
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new Gson();
+                Event[] events = gson.fromJson(response, Event[].class);
+                eventAdapter.setEvents(events);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(request);
+    }
+
+    class EventAdapter extends RecyclerView.Adapter<EventViewHolder>{
+        private Event[] events;
+
+        public Event[] getEvents() {
+            return events;
+        }
+
+        public void setEvents(Event[] events) {
+            this.events = events;
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public EventViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.viewholder_event,viewGroup,false);
+            EventViewHolder eventViewHolder = new EventViewHolder(view);
+
+            return eventViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull EventViewHolder eventViewHolder, int i) {
+            Event event=events[i];
+            eventViewHolder.textView.setText(event.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            if(events==null){
+                return 0;
+            }else {
+                return events.length;
+            }
+        }
+    }
+    class EventViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textView;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imgviewholder);
+            textView = itemView.findViewById(R.id.tvtitle);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/leapkh/ckcc/MainActivity.java
@@ -0,0 +1,63 @@
+package com.leapkh.ckcc;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId()==R.id.mnu_home){
+                    onHomeClick();
+                }else if(menuItem.getItemId()==R.id.mnu_event){
+                   onEventClick();
+                }else {
+                    onProfileClick();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void onHomeClick(){
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.layout_main, homeFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void onEventClick(){
+        EventsFragment eventsFragment = new EventsFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.layout_main, eventsFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void onProfileClick(){
+        ProfileFragment profileFragment = new ProfileFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.layout_main, profileFragment);
+        fragmentTransaction.commit();
+    }
+
+}
