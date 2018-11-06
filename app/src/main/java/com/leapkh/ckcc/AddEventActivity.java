9c5a853b7c3fc7c69e05590df0a9faa1cc4301b6
leapkh <leapkh@yahoo.com>
2018-11-06 20:12:48 +0700

    Using cloud storage.

--- app/src/main/java/com/leapkh/ckcc/AddEventActivity.java
+++ app/src/main/java/com/leapkh/ckcc/AddEventActivity.java
@@ -1,11 +1,17 @@
 package com.leapkh.ckcc;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -16,28 +22,70 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class AddEventActivity extends AppCompatActivity {
 
+    private final int GALLERY_REQUEST_CODE = 1;
+
+    private ImageView imgEvent;
     private ProgressBar progressBar;
+    private Bitmap selectedImage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_event);
+        imgEvent = findViewById(R.id.img_event);
         progressBar = findViewById(R.id.progressBar);
     }
 
     public void onSaveButtonClick(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Upload image to Firebase storage
+        // Create reference
+        String fileLocation = "/images/" + System.currentTimeMillis() + ".png";
+        final StorageReference imageRef = FirebaseStorage.getInstance().getReference(fileLocation);
+
+        // Convert bitmap to byte array
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        selectedImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        // Start upload
+        UploadTask uploadTask = imageRef.putBytes(byteArray);
+        Task<Uri> urlTastk = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return imageRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                String imageUrl = task.getResult().toString();
+                addEventToFirestore(imageUrl);
+            }
+        });
+
+
+    }
+
+    private void addEventToFirestore(String imageUrl){
         EditText etxtTitle = findViewById(R.id.etxt_title);
         EditText etxtDate = findViewById(R.id.etxt_date);
         EditText etxtLocation = findViewById(R.id.etxt_location);
@@ -76,14 +124,13 @@ public class AddEventActivity extends AppCompatActivity {
         */
 
         // Add event to Firestore
-        progressBar.setVisibility(View.VISIBLE);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         Map<String, Object> event = new HashMap<>();
         event.put("title", title);
         event.put("date", date);
         event.put("location", location);
-        event.put("imageUrl", "http://test.js-cambodia.com/ckcc/images/android.png");
+        event.put("imageUrl", imageUrl);
 
         /*db.collection("events").add(event).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
             @Override
@@ -112,6 +159,25 @@ public class AddEventActivity extends AppCompatActivity {
         });
     }
 
+    public void onEventImageViewClick(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                imgEvent.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
 
