53dcc45e537b318066b6332a519e8887a82b8d0e
leapkh <leapkh@yahoo.com>
2018-11-27 20:23:39 +0700

    Getting user current location.

--- app/src/main/java/com/leapkh/ckcc/AddEventActivity.java
+++ app/src/main/java/com/leapkh/ckcc/AddEventActivity.java
@@ -1,12 +1,17 @@
 package com.leapkh.ckcc;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +27,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -40,10 +50,14 @@ import java.util.Map;
 public class AddEventActivity extends AppCompatActivity {
 
     private final int GALLERY_REQUEST_CODE = 1;
+    private final int LOCATION_PERMISSION_REQUEST_CODE = 2;
 
     private ImageView imgEvent;
     private ProgressBar progressBar;
     private Bitmap selectedImage;
+    private EditText etxtCoornidate;
+
+    private FusedLocationProviderClient locationProviderClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,6 +66,72 @@ public class AddEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_event);
         imgEvent = findViewById(R.id.img_event);
         progressBar = findViewById(R.id.progressBar);
+        etxtCoornidate = findViewById(R.id.etxt_coordinate);
+
+        loadUserCurrentLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadUserCurrentLocation();
+            } else {
+                Toast.makeText(this, "This app cannot work properly without location permission. You should allow it.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void loadUserCurrentLocation() {
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+        Task<Location> locationTask = locationProviderClient.getLastLocation();
+        locationTask.addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        etxtCoornidate.setText(location.getLatitude() + ", " + location.getLongitude());
+                    } else {
+                        Toast.makeText(AddEventActivity.this, "Load last known location not found.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(AddEventActivity.this, "Load last known location fail.", Toast.LENGTH_LONG).show();
+                    Log.d("ckcc", "Load last known location fail: " + task.getException());
+                }
+                requestLocationUpdate();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestLocationUpdate() {
+        Log.d("ckcc", "requestLocationUpdate");
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.d("ckcc", "Location updated.");
+                Toast.makeText(AddEventActivity.this, "Location updated.", Toast.LENGTH_LONG).show();
+                Location updatedLocation = locationResult.getLastLocation();
+                Log.d("ckcc", updatedLocation.getLatitude() + ", " + updatedLocation.getLongitude());
+                etxtCoornidate.setText(updatedLocation.getLatitude() + ", " + updatedLocation.getLongitude());
+                locationProviderClient.removeLocationUpdates(this);
+            }
+        };
+
+        locationProviderClient.requestLocationUpdates(request, locationCallback, null);
     }
 
     public void onSaveButtonClick(View view) {
@@ -85,7 +165,7 @@ public class AddEventActivity extends AppCompatActivity {
 
     }
 
-    private void addEventToFirestore(String imageUrl){
+    private void addEventToFirestore(String imageUrl) {
         EditText etxtTitle = findViewById(R.id.etxt_title);
         EditText etxtDate = findViewById(R.id.etxt_date);
         EditText etxtLocation = findViewById(R.id.etxt_location);
@@ -159,7 +239,7 @@ public class AddEventActivity extends AppCompatActivity {
         });
     }
 
-    public void onEventImageViewClick(View view){
+    public void onEventImageViewClick(View view) {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
         startActivityForResult(intent, GALLERY_REQUEST_CODE);
@@ -169,7 +249,7 @@ public class AddEventActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK) {
             try {
                 selectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
                 imgEvent.setImageBitmap(selectedImage);
